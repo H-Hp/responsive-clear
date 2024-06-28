95cc91615d8e03c4cf4c3649d096306b17319e8b
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-06-28 21:12:52 +0900

    feature:canonicalでURL正規化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { GoogleAnalytics } from "./GoogleAnalytics";
 export const metadata = {
   title: 'Responsive Clear',
   description: '簡単・高速なレスポンシブデザインチェックツール。複数デバイス同時プレビュー、カスタムサイズ設定可能。ウェブデザイナー・開発者向けの必須ツールでサイトの表示を最適化。無料で今すぐ使えます！',
+  alternates: {
+    canonical: 'https://responsive-clear.online',
+  },
   openGraph: {
     title: 'ResponsiveClear',
     description: 'サイトのレスポンシブチェックツール',
