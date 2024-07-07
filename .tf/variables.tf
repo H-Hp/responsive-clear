0b5733862992a3eb277736804ffc69932680d112
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-07-07 14:46:04 +0900

    feat: variables.tf作成

---
+++ .tf/variables.tf
@@ -0,0 +1,19 @@
+# variables.tf
+
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "vpc cidrblock"
+}
+
+variable "subnet_cidr" {
+  type        = string
+  description = "public subnet cidr"
+}
