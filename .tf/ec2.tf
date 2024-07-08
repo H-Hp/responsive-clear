8329cb636cb221478ee762dee1f2405a950e0575
Hiroki_Macbook <yarebadekirusa3333@yahoo.co.jp>
2024-07-08 23:13:37 +0900

    feat: ansibleでEC2へgithubからpullしてnext.jsのデプロイまで

--- .tf/ec2.tf
+++ .tf/ec2.tf
@@ -1,12 +1,27 @@
 # ec2.tf
+
+resource "null_resource" "generate_ssh_key" {
+ provisioner "local-exec" {
+    command = "ssh-keygen -t rsa -f ec2-keypair -y"
+  }
+
+  # キーが既に存在する場合は再生成しないようにする
+  triggers = {
+    key_file = "~/.ssh/ec2-keypair"
+  }
+}
+
+
 # EC2 keypair
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh_key"
   public_key = file("~/.ssh/ec2-keypair.pub")
+  #public_key = var.ssh_public_key_path
 }
+
 # EC2 instance
 resource "aws_instance" "main" {
-  ami                         = "ami-0c3fd0f5d33134a76"
+  ami                         = "ami-0e0820ad173f20fbb"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public.id
   associate_public_ip_address = true
@@ -18,4 +33,30 @@ resource "aws_instance" "main" {
     Project = var.project
     Env     = var.environment
   }
+
+
+    provisioner "remote-exec" {
+      inline = [
+        "echo 'Wait until SSH is ready'",
+        #"sudo yum update -y",  # Amazon Linux 2の場合
+        "sudo yum install -y python3"
+      ]
+
+      connection {
+        type        = "ssh"
+        user        = "ec2-user"
+        private_key = file("~/.ssh/ec2-keypair")
+        #private_key = var.ssh_private_key_path
+        host        = self.public_ip
+      }
+    }
+
+  provisioner "local-exec" {
+    #command = "ansible-playbook -i '${self.public_ip},' --private-key ${path.module}/.ssh/ec2-keypair setup_nextjs.yml"
+    #command = "ansible-playbook -i '${self.public_ip},' --private-key file('~/.ssh/ec2-keypair') setup_nextjs.yml"
+    #command = "ansible-playbook -i '${self.public_ip},' --private-key ${var.ssh_private_key_path} setup_nextjs.yml"
+    command = "ansible-playbook -i '${self.public_ip},' -u ec2-user --private-key ${var.ssh_private_key_path} setup_nextjs.yml"
+
+  }
+
 }
--- .tf/variables.tf
+++ .tf/variables.tf
@@ -17,3 +17,12 @@ variable "subnet_cidr" {
   type        = string
   description = "public subnet cidr"
 }
+
+variable "ssh_private_key_path" {
+  description = "Path to the SSH key"
+  type        = string
+}
+variable "ssh_public_key_path" {
+  description = "Path to the SSH key"
+  type        = string
+}
